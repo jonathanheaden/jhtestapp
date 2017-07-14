f0b59c2babde0ef7794d376d4a084d9b820f2965
Jonathan Headen <jonathan.headen@outlook.com>
2017-07-14 17:23:01 +1000

    update things that are said

--- controllers/shared.js
+++ controllers/shared.js
@@ -4,10 +4,14 @@ var thingsthataresaid = [
     'Sorry I was on Mute',
     'Can we take this offline?',
     '[Animal or Child Noises]',
+    'Are you still there _____ ??',
     'Can everyone see my Screen',
     'Can you email that to everyone?',
     'I need to jump on another call',
     'can everyone go on Mute?',
+    'OK lets kick off',
+    'sorry I\'m late [lame excuse]',
+    'I didn\'t catch that .. can you repeat?',
     'No it is still loading'
 ];
 
