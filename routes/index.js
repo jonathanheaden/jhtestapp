2b512e48ce3b5f95b4d13b59e61af6b2bbcea2ea
Jonathan Headen <jonathan.headen@outlook.com>
2017-07-18 13:54:04 +1000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var ctrlPlayers = require('../controllers/players');
 
 /* GET home page. */
 router.get('/', ctrlHome.landing);
+router.get('/game/', ctrlPlayers.readgame);
 router.get('/game/:playerid', ctrlPlayers.readgame);
 router.get('/players/:playerid', ctrlPlayers.getplayer);
 router.put('/players/:playerid/:phrase', ctrlPlayers.putplayer);
